1d36ead37d2b7dd5c316fb8aa7eba31353d3ec37
Ace SLS <acesls@protonmail.com>
2026-07-05 12:03:37 +0200

    feat(schema-grabber): Add batch mode Automatically creates empty stat files for all users from loginusers.vdf When AppId 0 is supplied it batch grabs schemas for all apps from libraryfolders.vdf

--- tools/schema-grabber/Program.cs
+++ tools/schema-grabber/Program.cs
@@ -43,12 +43,127 @@ namespace SchemaGrabber
     class SchemaGrabber
     {
         //Thanks Parasitic-Hollow for discovering this endpoint
-        public const string ReviewUrl = "https://store.steampowered.com/appreviews/{appId}?json=1&filter=recent&language=all&purchase_type=all&num_per_page=100";
+        public const string ReviewUrl = "https://store.steampowered.com/appreviews/{appId}?json=1&filter=recent&language=all&purchase_type=all&num_per_page=10";
 
         public readonly string GuardDataDir = Path.Combine(Environment.CurrentDirectory, "Sentries");
         public readonly string TicketDir = Path.Combine(Environment.CurrentDirectory, "Tickets");
 
-        public readonly string StatsDir = Path.Combine(Environment.GetEnvironmentVariable("HOME"), ".steam/steam/appcache/stats");
+        public string GetSteamDir()
+        {
+            var home = Environment.GetEnvironmentVariable("HOME");
+            if (home == null)
+            {
+                Console.WriteLine("Failed to get home directory!");
+                Environment.Exit(1);
+            }
+
+            var steamPaths = new[]
+            {
+                ".steam/steam",
+                ".var/app/com.valvesoftware.Steam/.steam/steam"
+            };
+
+            var steamPath = "";
+
+            foreach (var path in steamPaths)
+            {
+                var combined = Path.Combine(home, path);
+                if (Directory.Exists(combined))
+                {
+                    steamPath = combined;
+                    break;
+                }
+            }
+
+            if (steamPath.Length < 1)
+            {
+                Console.WriteLine("Failed to find steam directory!");
+                Environment.Exit(1);
+            }
+
+            return steamPath;
+        }
+
+        public string GetStatsDir()
+        {
+            var steamPath = GetSteamDir();
+            var statsDir = Path.Combine(steamPath, "appcache/stats");
+
+            if (!Directory.Exists(statsDir))
+            {
+                if (!Directory.CreateDirectory(statsDir).Exists)
+                {
+                    Console.WriteLine($"Failed to create {statsDir}");
+                    Environment.Exit(1);
+                }
+            }
+
+            return statsDir;
+        }
+
+        public List<ulong> GetUsers()
+        {
+            var users = new List<ulong>();
+
+            var loginVdf = Path.Combine(GetSteamDir(), "config", "loginusers.vdf");
+            if (!File.Exists(loginVdf))
+            {
+                Console.WriteLine($"{loginVdf} does not exist! Will generate schemas only for the logged in account");
+                return users;
+            }
+
+            var text = File.ReadAllText(loginVdf);
+
+            //var re = new Regex("(?<=^\t\")[0-9]+(?=\")"); //Ain't work
+            var re = new Regex("(?<=\t\")[0-9]{17}(?=\")");
+            var matches = re.Matches(text);
+
+            foreach (var match in matches)
+            {
+                var steamIdStr = match.ToString();
+                if (steamIdStr == null)
+                {
+                    continue;
+                }
+
+                var accountId = ulong.Parse(steamIdStr) & 0xFFFFFFFF;
+                users.Add(accountId);
+                Console.WriteLine($"Found user {accountId}");
+            }
+
+            return users;
+        }
+
+        public List<uint> GetInstalledGames()
+        {
+            var games = new List<uint>();
+
+            var libraryVdf = Path.Combine(GetSteamDir(), "config", "libraryfolders.vdf");
+            if (!File.Exists(libraryVdf))
+            {
+                Console.WriteLine($"{libraryVdf} does not exist!");
+                return games;
+            }
+
+            var text = File.ReadAllText(libraryVdf);
+
+            var re = new Regex("(?<=\t\t\t\")[0-9]+(?=\")");
+            var matches = re.Matches(text);
+
+            foreach (var match in matches)
+            {
+                var appIdStr = match.ToString();
+                if (appIdStr == null)
+                {
+                    continue;
+                }
+
+                games.Add(uint.Parse(appIdStr));
+                Console.WriteLine($"Found {appIdStr} in library");
+            }
+
+            return games;
+        }
 
         protected bool finished = false;
         protected CallbackManager callbackManager;
@@ -173,10 +288,23 @@ namespace SchemaGrabber
             Console.WriteLine($"Logged out from {Username}");
         }
 
-        protected void OnAccountInfo(SteamUser.AccountInfoCallback cb)
+        protected async void OnAccountInfo(SteamUser.AccountInfoCallback cb)
         {
             Console.WriteLine("Account Info received! Grabbing schema...");
-            GetSchema();
+
+            if (TargetAppId > 0)
+            {
+                await GetSchema(TargetAppId);
+            }
+            else
+            {
+                foreach (var appId in GetInstalledGames())
+                {
+                    await GetSchema(appId);
+                }
+            }
+
+            finished = true;
         }
 
         public AsyncJob<GetUserStatsCallback> GetUserStats(ulong steamId, uint appId)
@@ -193,9 +321,9 @@ namespace SchemaGrabber
             return new AsyncJob<GetUserStatsCallback>(Client, msg.SourceJobID);
         }
 
-        protected async void GetSchema()
+        protected async Task GetSchema(uint appId)
         {
-            var uri = ReviewUrl.Replace("{appId}", TargetAppId.ToString());
+            var uri = ReviewUrl.Replace("{appId}", appId.ToString());
             Console.WriteLine($"Downloading {uri}");
 
             using (var wc = new HttpClient())
@@ -214,7 +342,7 @@ namespace SchemaGrabber
                     }
 
                     var steamId = ulong.Parse(steamIdStr);
-                    var resp = await GetUserStats(steamId, TargetAppId);
+                    var resp = await GetUserStats(steamId, appId);
                     var body = resp.body;
 
                     Console.WriteLine($"GetUserStatsResponse {resp.body.eresult}");
@@ -229,24 +357,33 @@ namespace SchemaGrabber
                         continue;
                     }
 
+                    var statsDir = GetStatsDir();
                     var accountId = User.SteamID & 0xffffffff;
-                    File.WriteAllBytes($"{StatsDir}/UserGameStatsSchema_{body.game_id}.bin", resp.body.schema);
+                    File.WriteAllBytes($"{statsDir}/UserGameStatsSchema_{body.game_id}.bin", resp.body.schema);
 
-                    var userStatsPath = $"{StatsDir}/UserGameStats_{accountId}_{body.game_id}.bin";
-                    if (!File.Exists(userStatsPath))
+                    var users = GetUsers();
+                    if (!users.Contains(accountId))
                     {
-                        File.WriteAllBytes(userStatsPath, new byte[]
-                        {
-                            0x00, 0x63, 0x61, 0x63, 0x68, 0x65, 0x00, 0x02, 0x63, 0x72, 0x63, 0x00, 0x00, 0x00, 0x00, 0x00,
-                            0x02, 0x50, 0x65, 0x6e, 0x64, 0x69, 0x6e, 0x67, 0x43, 0x68, 0x61, 0x6e, 0x67, 0x65, 0x73, 0x00,
-                            0x00, 0x00, 0x00, 0x00, 0x08, 0x08
-                        });
+                        users.Add(accountId);
                     }
+
+                    foreach (var user in users)
+                    {
+                        var userStatsPath = $"{statsDir}/UserGameStats_{user}_{body.game_id}.bin";
+                        if (!File.Exists(userStatsPath))
+                        {
+                            File.WriteAllBytes(userStatsPath, new byte[]
+                            {
+                                0x00, 0x63, 0x61, 0x63, 0x68, 0x65, 0x00, 0x02, 0x63, 0x72, 0x63, 0x00, 0x00, 0x00, 0x00, 0x00,
+                                0x02, 0x50, 0x65, 0x6e, 0x64, 0x69, 0x6e, 0x67, 0x43, 0x68, 0x61, 0x6e, 0x67, 0x65, 0x73, 0x00,
+                                0x00, 0x00, 0x00, 0x00, 0x08, 0x08
+                            });
+                        }
+                    }
+
                     break;
                 }
             }
-
-            finished = true;
         }
 
         public void CallbackLoop()
