f62d97c2e49347b9fd400811eca65264164ce626
Ace SLS <acesls@protonmail.com>
2026-07-11 16:08:24 +0200

    fix(curl): Replace libcurl with extern curl binary It's necessary because SteamOS seems broken. Curling certain URLs will crash inside libssl.3.so (might have to do with broken certs, idk for sure).
    Big thanks to Parasitic-Hollow once again for coming up with this fix

--- src/curl.cpp
+++ src/curl.cpp
@@ -2,27 +2,96 @@
 
 #include "log.hpp"
 
-#include <curl/curl.h>
-#include <curl/easy.h>
+#include <cstdlib>
+#include <sstream>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-static CURL* curl = nullptr;
-
-static size_t writeCallback(const char* content, size_t size, size_t memberSize, std::string* data)
-{
-	data->append(content, size * memberSize);
-	return size * memberSize;
-}
-
+//Spawn an external instance of curl, read it's stdout into out and return it's exit code
+//It's necessary because SteamOS seems broken. Curling certain URLs
+//will crash inside libssl.3.so (might have to do with broken certs, idk for sure).
 int Curl::getString(const char* url, std::string& out)
 {
-	curl = curl_easy_init();
-	curl_easy_setopt(curl, CURLOPT_URL, url);
-	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
-	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &out);
+	g_pLog->debug("Curl::getString(%s)\n", url);
 
-	auto res = curl_easy_perform(curl);
+	int pipefd[2];
 
-	curl_easy_cleanup(curl);
+	if (pipe(pipefd) == -1)
+	{
+		g_pLog->debug("Failed to create pipe!\n");
+		return 1;
+	}
 
-	return res;
+	g_pLog->debug("Created pipe %i : %i\n", pipefd[0], pipefd[1]);
+
+	constexpr static const char* env[] =
+	{
+		"PATH='/usr/bin:/bin'",
+		nullptr
+	};
+
+	const char* args[] =
+	{
+		"-s",
+		url,
+		nullptr
+	};
+
+	pid_t pid = fork();
+	if (pid == -1)
+	{
+		g_pLog->debug("Failed to fork!\n");
+		return 1;
+	}
+
+	if (pid == 0)
+	{
+		if (dup2(pipefd[1], STDOUT_FILENO) == -1)
+		{
+			g_pLog->debug("Failed to dup2!\n");
+			exit(1);
+		}
+
+		//No need for reading
+		close(pipefd[0]);
+		close(pipefd[1]);
+
+		execve("/bin/curl", const_cast<char**>(args), const_cast<char**>(env));
+		execve("/usr/bin/curl", const_cast<char**>(args), const_cast<char**>(env));
+
+		g_pLog->debug("Failed to execv curl!\n");
+		exit(1);
+	}
+
+	//No need for writing
+	close(pipefd[1]);
+
+	g_pLog->debug("Child PID %i\n", pid);
+
+	std::ostringstream bufSS;
+	char buf[128];
+	int numRead;
+
+	while((numRead = read(pipefd[0], buf, sizeof(buf))) > 0)
+	{
+		bufSS << std::string(buf, numRead);
+	}
+
+	int status;
+	if(waitpid(pid, &status, 0) == -1)
+	{
+		return 1;
+	}
+
+	if(!WIFEXITED(status))
+	{
+		return 1;
+	}
+
+	g_pLog->debug("Exit Status: %i\n", status);
+
+	out = bufSS.str();
+
+	return status;
 }
