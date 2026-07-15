7d62c681be2e92fe047ee7a90fff325029e0dfd2
Ace SLS <acesls@protonmail.com>
2026-07-15 19:59:37 +0200

    fix(config): Prevent infinite recursion happening in shouldExcludeApp Seems like some parent reference their children as parents...

--- src/config.cpp
+++ src/config.cpp
@@ -337,24 +337,27 @@ bool CConfig::shouldExcludeAppId(uint32_t appId, bool ignoreAdditionalApps)
 		bool found = appIds.get().contains(appId);
 		exclude = (!isAddedAppId(appId) || ignoreAdditionalApps) && ((whitelist && !found) || (!whitelist && found));
 
-		//Might be worth to check for APPTYPE_DLC, but knowing Valve & individual gamedevs
-		//surely not every DLC will be tagged as such
-		char chParent[16];
-		const int len = g_pClientApps ? g_pClientApps->getAppData(appId, "parent", chParent, sizeof(chParent)) : 0;
-		if (len > 0)
+		if (!ignoreAdditionalApps)
 		{
-			//g_pLog->debug("AppId %i, parent %s (%i)\n", appId, chParent, len);
-			uint32_t parentId = std::stoul(chParent);
+			//Might be worth to check for APPTYPE_DLC, but knowing Valve & individual gamedevs
+			//surely not every DLC will be tagged as such
+			char chParent[16];
+			const int len = g_pClientApps ? g_pClientApps->getAppData(appId, "parent", chParent, sizeof(chParent)) : 0;
+			if (len > 0)
+			{
+				//g_pLog->debug("AppId %i, parent %s (%i)\n", appId, chParent, len);
+				uint32_t parentId = std::stoul(chParent);
 
-			if (whitelist && !shouldExcludeAppId(parentId, true))
-			{
-				//g_pLog->debug("Override exclude %i with false, because parent %u isn't excluded\n", exclude, parentId);
-				exclude = false;
-			}
-			else if(!whitelist && shouldExcludeAppId(parentId, true))
-			{
-				//g_pLog->debug("Override exclude %i with true, because parent %u is excluded\n", exclude, parentId);
-				exclude = true;
+				if (whitelist && !shouldExcludeAppId(parentId, true))
+				{
+					//g_pLog->debug("Override exclude %i with false, because parent %u isn't excluded\n", exclude, parentId);
+					exclude = false;
+				}
+				else if(!whitelist && shouldExcludeAppId(parentId, true))
+				{
+					//g_pLog->debug("Override exclude %i with true, because parent %u is excluded\n", exclude, parentId);
+					exclude = true;
+				}
 			}
 		}
 	}
